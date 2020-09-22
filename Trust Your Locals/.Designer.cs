0437c3e2e33a32f3214c42c75a08221465507e90
Kastuks <kkostass9@gmail.com>
2020-09-22 09:26:43 +0300

    added button and datagrid

--- Trust Your Locals/.Designer.cs
+++ Trust Your Locals/.Designer.cs
@@ -11,8 +11,25 @@ namespace Trust_Your_Locals
         private Button button1;
         private TextBox textBox1;
         private ComboBox comboBox1;
-       
-    
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private Database1DataSetTableAdapters.SellerTableAdapter sellerTableAdapter;
+        private Database1DataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private Database1DataSet database1DataSet1;
+        private BindingSource sellerBindingSource;
+        private BindingSource fKProductsShopI25869641BindingSource;
+        private BindingSource sellerBindingSource1;
+        private BindingSource productsBindingSource;
+        private DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn adressDataGridViewTextBoxColumn;
+        private Database1DataSet database1DataSet2;
+        private BindingSource productsBindingSource1;
+        private ToolStrip fillToolStrip;
+        private ToolStripButton fillToolStripButton;
+
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -38,52 +55,221 @@ namespace Trust_Your_Locals
         /// </summary>
         private void InitializeComponent()
         {
-       
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.productsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet2 = new Trust_Your_Locals.Database1DataSet();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new Trust_Your_Locals.Database1DataSet();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sellerBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.fKProductsShopI25869641BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sellerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet1 = new Trust_Your_Locals.Database1DataSet();
+            this.sellerTableAdapter = new Trust_Your_Locals.Database1DataSetTableAdapters.SellerTableAdapter();
+            this.productsTableAdapter = new Trust_Your_Locals.Database1DataSetTableAdapters.ProductsTableAdapter();
+            this.fillToolStrip = new System.Windows.Forms.ToolStrip();
+            this.fillToolStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sellerBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKProductsShopI25869641BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sellerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).BeginInit();
+            this.fillToolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(381, 150);
+            this.button1.DataBindings.Add(new System.Windows.Forms.Binding("ContextMenuStrip", this.productsBindingSource1, "Name", true));
+            this.button1.Location = new System.Drawing.Point(339, 120);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 53);
+            this.button1.Size = new System.Drawing.Size(99, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Get Data";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += Button1_Click;
+            // 
+            // productsBindingSource1
+            // 
+            this.productsBindingSource1.DataMember = "Products";
+            this.productsBindingSource1.DataSource = this.database1DataSet2;
+            // 
+            // database1DataSet2
+            // 
+            this.database1DataSet2.DataSetName = "Database1DataSet";
+            this.database1DataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(345, 73);
+            this.textBox1.Location = new System.Drawing.Point(307, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 40);
+            this.textBox1.Size = new System.Drawing.Size(154, 33);
             this.textBox1.TabIndex = 1;
             // 
             // comboBox1
             // 
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.productsBindingSource, "Name", true));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(111, 73);
+            this.comboBox1.Location = new System.Drawing.Point(99, 67);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 28);
+            this.comboBox1.Size = new System.Drawing.Size(144, 24);
             this.comboBox1.TabIndex = 2;
-            this.comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
-           
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.AutoEllipsis = true;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(507, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Show marketplaces";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.adressDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.sellerBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(99, 232);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(837, 217);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name:";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name:";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // adressDataGridViewTextBoxColumn
+            // 
+            this.adressDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.adressDataGridViewTextBoxColumn.DataPropertyName = "Adress:";
+            this.adressDataGridViewTextBoxColumn.HeaderText = "Adress:";
+            this.adressDataGridViewTextBoxColumn.MinimumWidth = 50;
+            this.adressDataGridViewTextBoxColumn.Name = "adressDataGridViewTextBoxColumn";
+            this.adressDataGridViewTextBoxColumn.ReadOnly = true;
+            this.adressDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // sellerBindingSource1
+            // 
+            this.sellerBindingSource1.DataMember = "Seller";
+            this.sellerBindingSource1.DataSource = this.database1DataSet;
+            // 
+            // fKProductsShopI25869641BindingSource
+            // 
+            this.fKProductsShopI25869641BindingSource.DataMember = "FK__Products__Shop I__25869641";
+            this.fKProductsShopI25869641BindingSource.DataSource = this.sellerBindingSource;
+            // 
+            // sellerBindingSource
+            // 
+            this.sellerBindingSource.DataMember = "Seller";
+            this.sellerBindingSource.DataSource = this.database1DataSet1;
+            // 
+            // database1DataSet1
+            // 
+            this.database1DataSet1.DataSetName = "Database1DataSet";
+            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sellerTableAdapter
+            // 
+            this.sellerTableAdapter.ClearBeforeFill = true;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // fillToolStrip
+            // 
+            this.fillToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.fillToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fillToolStripButton});
+            this.fillToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fillToolStrip.Name = "fillToolStrip";
+            this.fillToolStrip.Size = new System.Drawing.Size(112, 25);
+            this.fillToolStrip.TabIndex = 5;
+            this.fillToolStrip.Text = "fillToolStrip";
+            // 
+            // fillToolStripButton
+            // 
+            this.fillToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillToolStripButton.Name = "fillToolStripButton";
+            this.fillToolStripButton.Size = new System.Drawing.Size(32, 22);
+            this.fillToolStripButton.Text = "Fill";
+            this.fillToolStripButton.Click += new System.EventHandler(this.fillToolStripButton_Click_1);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(1058, 521);
+            this.Controls.Add(this.fillToolStrip);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Trust Your Locals";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sellerBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKProductsShopI25869641BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sellerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).EndInit();
+            this.fillToolStrip.ResumeLayout(false);
+            this.fillToolStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +303,96 @@ namespace Trust_Your_Locals
             
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet2.Products' table. You can move, or remove it, as needed.
+            try
+            {
+                this.productsTableAdapter.Fill(this.database1DataSet2.Products);
+            }
+            catch (Exception ex) { }
+                // TODO: This line of code loads data into the 'database1DataSet1.Products' table. You can move, or remove it, as needed.
+            // TODO: This line of code loads data into the 'database1DataSet1.Seller' table. You can move, or remove it, as needed.
+            // TODO: This line of code loads data into the 'database1DataSet.Products' table. You can move, or remove it, as needed.
+            // TODO: This line of code loads data into the 'database1DataSet.Products' table. You can move, or remove it, as needed.
+            // TODO: This line of code loads data into the 'database1DataSet.Seller' table. You can move, or remove it, as needed.
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.productsTableAdapter.FillBy(this.database1DataSet.Products);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillByToolStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void fillBy1ToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.productsTableAdapter.FillBy1(this.database1DataSet.Products);
+            }
+            catch (System.Exception ex)
+            {
+          //      System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillBy2ToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.productsTableAdapter.FillBy2(this.database1DataSet.Products);
+            }
+            catch (System.Exception ex)
+            {
+          //      System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sellerTableAdapter.Fill(this.database1DataSet.Seller);
+            }
+            catch (System.Exception ex)
+            {
+          //      System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void fillToolStripButton_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                this.sellerTableAdapter.Fill(this.database1DataSet.Seller);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
     }
 }
 
--- Trust Your Locals/Database1DataSet.Designer.cs
+++ Trust Your Locals/Database1DataSet.Designer.cs
@@ -1239,15 +1239,23 @@ namespace Trust_Your_Locals.Database1DataSetTableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[4];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT [Shop ID], Price, Name FROM dbo.Products";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
             this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[1].Connection = this.Connection;
-            this._commandCollection[1].CommandText = "SELECT [Shop ID], Price, Name FROM dbo.Products";
+            this._commandCollection[1].CommandText = "SELECT Name\r\nFROM     Products";
             this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[2] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[2].Connection = this.Connection;
+            this._commandCollection[2].CommandText = "SELECT [Shop ID], Price, Name\r\nFROM     Products";
+            this._commandCollection[2].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[3] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[3].Connection = this.Connection;
+            this._commandCollection[3].CommandText = "SELECT [Shop ID], Price, Name FROM dbo.Products";
+            this._commandCollection[3].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -1280,6 +1288,32 @@ namespace Trust_Your_Locals.Database1DataSetTableAdapters {
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
         public virtual int FillBy(Database1DataSet.ProductsDataTable dataTable) {
             this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBy1(Database1DataSet.ProductsDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[2];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBy2(Database1DataSet.ProductsDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[3];
             if ((this.ClearBeforeFill == true)) {
                 dataTable.Clear();
             }
