d0a5fbaea3bb14f23a56881665d1bbe3d5378ce3
simasnau <simasnau@gmail.com>
2020-09-26 11:34:30 +0300

    Added SQLConnectionHandler Class

--- Trust Your Locals/.Designer.cs
+++ Trust Your Locals/.Designer.cs
@@ -8,11 +8,11 @@ namespace Trust_Your_Locals
 {
     partial class Form1
     {
-        private Button button1;
         private TextBox textBox1;
         private ComboBox comboBox1;
-       
-    
+        private Button button1;
+
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -38,71 +38,63 @@ namespace Trust_Your_Locals
         /// </summary>
         private void InitializeComponent()
         {
-       
-            this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(381, 150);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Get Data";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += Button1_Click;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(345, 73);
+            this.textBox1.Location = new System.Drawing.Point(307, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 40);
+            this.textBox1.Size = new System.Drawing.Size(154, 33);
             this.textBox1.TabIndex = 1;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(111, 73);
+            this.comboBox1.Location = new System.Drawing.Point(101, 58);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 28);
+            this.comboBox1.Size = new System.Drawing.Size(144, 24);
             this.comboBox1.TabIndex = 2;
             this.comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
-           
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Find this product";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Trust Your Locals";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e) {
-        //When selected item changes, textBox changes.
-            textBox1.Text = comboBox1.SelectedValue.ToString();
-        }
-
-        private void Button1_Click(object sender, System.EventArgs e) { 
-        //This Function connects to local database on button press and populates the combo box
-            try {
-                // INSERT YOUR OWN CONNECTION STRING
-                string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Programavimas\\VU\\2 Kursas\\PSI\\test\\Trust Your Locals\\Trust Your Locals\\Database1.mdf;Integrated Security=True";
-                SqlConnection conn = new SqlConnection(connectionString);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLConnectionHandler.MakeConnection();
                 string query = "SELECT DISTINCT Name FROM Products";
-                conn.Open();
-                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                SqlDataAdapter da = new SqlDataAdapter(query, SQLConnectionHandler.GetConnection());
                 DataSet ds = new DataSet();
                 da.Fill(ds, "Products");
                 comboBox1.DisplayMember = "Name";
@@ -110,12 +102,24 @@ namespace Trust_Your_Locals
                 comboBox1.DataSource = ds.Tables["Products"];
 
             }
-            catch (SqlException a) {
+            catch (SqlException a)
+            {
                 Console.WriteLine(a);
             }
-            
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+        //When selected item changes, textBox changes.
+            textBox1.Text = comboBox1.SelectedValue.ToString();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             
         }
+
 
     }
 }
