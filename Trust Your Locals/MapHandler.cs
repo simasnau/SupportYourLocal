a582986d426521c488324359a397afdc345acc18
Kastuks <kkostass9@gmail.com>
2020-10-12 15:09:02 +0300

    Added new Form3 to rate farmers shops.

--- Trust Your Locals/MapHandler.cs
+++ Trust Your Locals/MapHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xamarin.Forms.Maps;
 
 namespace Trust_Your_Locals
 {
@@ -14,6 +15,30 @@ namespace Trust_Your_Locals
         {
             this.webBrowser = webBrowser;
         }
+
+  /*      public void pins(String adresas, String pavadinimas)
+        {
+            Pin pin = new Pin();
+            {
+                Label = pavadinimas;
+                Adress = adresas;
+                Type = PinType.Place();
+                Position = new Position(adresas);
+            }
+        }*/
+
+        public void addPin()
+            {
+     /*       string product = "'" + cb.SelectedValue.ToString() + "'";
+            string sqlQuery = "SELECT Name, Price, \"Name:\", \"Adress:\" FROM Products, Seller WHERE \"Shop ID\"=ID";
+            if (product != "'Visi'") sqlQuery += " AND Name=" + product;
+
+            DataTable dt = new DataTable();
+            SQLConnectionHandler.MakeConnection();
+            SqlDataAdapter da = new SqlDataAdapter(sqlQuery, SQLConnectionHandler.GetConnection());
+            da.Fill(dt);
+            dgv.DataSource = dt;
+       */ }
 
         public void showAdress(string adresas)
         {
@@ -26,6 +51,7 @@ namespace Trust_Your_Locals
                     queryaddress.Append(adresas + "," + "+");
                 }
                 webBrowser.Navigate(queryaddress.ToString());
+               
             }
             catch (Exception ex)
             {
