cd4bc9c395d02664bb98eb7176afa24502bdf326
Kastuks <kkostass9@gmail.com>
2020-10-25 12:01:52 +0200

    Changed location of Rate Seller in Form1

--- Trust Your Locals/.Designer.cs
+++ Trust Your Locals/.Designer.cs
@@ -163,7 +163,7 @@ namespace Trust_Your_Locals
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(377, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(192, 41);
             this.button1.TabIndex = 8;
