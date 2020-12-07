a858134c93f1d9db0bae868ba4ee92749e22c596
simasnau <simasnau@gmail.com>
2020-12-07 23:54:16 +0200

    Added distance updating every 5 seconds in productView

--- SYL Mobile/SYL Mobile/ViewModels/ProductViewModel.cs
+++ SYL Mobile/SYL Mobile/ViewModels/ProductViewModel.cs
@@ -3,6 +3,8 @@ using SYL_Mobile.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -15,7 +17,11 @@ namespace SYL_Mobile.ViewModels
 
         public string sellerName { get; set; }
 
-        public string Distance { get; set; }
+        private string distance;
+        public string Distance {
+            get { return distance; }
+            set { SetProperty(ref distance, value); }
+        }
 
         public Position position { get; set; }
 
@@ -28,8 +34,21 @@ namespace SYL_Mobile.ViewModels
             Distance = p.distance + " km";
             sellerName = p.sellerName;
             PlaceOrderCommand = new Command(async () => await App.Current.MainPage.Navigation.PushAsync(new OrderAddPage(p)));
-            
+
+            var timer = new Timer((e) => UpdateDistance(), null, TimeSpan.Zero, TimeSpan.FromSeconds(5)); 
+
         }
+
+        async void UpdateDistance()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(10));
+            var location = await Geolocation.GetLocationAsync(request, new CancellationTokenSource().Token);
+            var curPos = new Position(location.Latitude, location.Longitude);
+            Distance = Convert.ToString(Math.Round(Xamarin.Forms.Maps.Distance.BetweenPositions(position, curPos).Kilometers, 2))+" km";
+
+        }
+
+
 
 
 
