f73b8a57b1345e0a8ecc61d0b5547baedf8e32c2
simasnau <simasnau@gmail.com>
2020-12-08 12:12:49 +0200

    Added Sorting to product list

--- SYL Mobile/SYL Mobile/Views/ItemsPage.xaml.cs
+++ SYL Mobile/SYL Mobile/Views/ItemsPage.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Forms;
 using SYL_Mobile.ViewModels;
 using SYL.Mobile.ViewModels;
 using SYL_Mobile.Models;
+using System.Diagnostics;
 
 namespace SYL_Mobile.Views
 {
@@ -40,5 +41,23 @@ namespace SYL_Mobile.Views
             await App.Current.MainPage.Navigation.PushAsync(new MapPage(ItemsListView.ItemsSource));
         }
 
+        async void SortByClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Sort By:", "Cancel", null, "Name", "Seller Name", "Price", "Distance");
+            Debug.WriteLine("Action: " + action);
+
+            var list = new List<Product> ((IEnumerable<Product>)ItemsListView.ItemsSource);
+            var newList=new List<Product>();
+            if (action.Equals("Name")) newList = list.OrderBy(x => x.name).ToList();
+            else if (action.Equals("Seller Name")) newList = list.OrderBy(x => x.sellerName).ToList();
+            else if (action.Equals("Price")) newList = list.OrderBy(x => x.price).ToList();
+            else if (action.Equals("Distance")) newList = list.OrderBy(x => x.distance).ToList();
+            else newList = list;
+
+            ItemsListView.ItemsSource = newList;
+
+
+
+        }
     }
 }
